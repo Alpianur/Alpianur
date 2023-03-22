0eff7aadf7acce27be39b3cdd440fce0bbc576d4
Alpianur XII <128021735+Alpianur@users.noreply.github.com>
2023-03-22 01:53:44 +0000

    Biodata Masing-Masing anak

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Alpianur</p>"
+  return "<p>Alpianur Ahmad Abdullah Al malik</p><p>Tempat, tanggal lahir : Lebanon, 28 Maret 2003</p><p>Alamat : Terantang, Seranau, Sampit</p><p>Jenis Kelamin : laki laki</p><p>Agama : Islam</p>"
+  
 
 
 if __name__ == "__main__":
